fe484daebc94826b6ce5ff66530de082a3302b09
Shane Rogers <rogerssh@tcd.ie>
2013-08-21 05:15:30 -0600

    sessions controller login spec corrected

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,13 +3,18 @@ require 'spec_helper'
 describe SessionsController do
 	describe 'POST#Create' do
 		context 'with valid attributes' do
-			it "renders the patient show when logged-in with valid attributes" do
-				patient = FactoryGirl.create(:patient, email: "banana@banana.com")
+			it "rendirects the patient show when logged-in with valid attributes" do
+				patient = FactoryGirl.create(:patient)
 				post :create, {email: patient.email, password: patient.password}
 				expect(response).to redirect_to(patient_path(patient))
 			end
 		end
 		context 'with invalid attributes' do
+			it "re-renders the index page" do
+				patient = FactoryGirl.create(:patient)
+				post :create, {email: patient.email, password: "banana"}
+				expect(response).to redirect_to(root_path)
+			end
 		end
 	end
 end
