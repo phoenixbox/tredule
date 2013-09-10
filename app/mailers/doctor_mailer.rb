b2700a09541d48b37096e4f787362b7f18e93f13
Shane Rogers <rogerssh@tcd.ie>
2013-09-10 05:12:50 -0600

    doctor signup confirmation link

--- app/mailers/doctor_mailer.rb
+++ app/mailers/doctor_mailer.rb
@@ -1,8 +1,13 @@
 class DoctorMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "admin@tredule.com"
 
   def password_reset(doctor)
     @doctor = doctor
     mail :to => doctor.email, :subject => "Password Reset"
+  end
+
+  def signup_confirmation(doctor)
+    @doctor = doctor
+    mail :to => doctor.email, :subject => "Signup Confirmation"
   end
 end
--- spec/mailers/doctor_mailer_spec.rb
+++ spec/mailers/doctor_mailer_spec.rb
@@ -5,11 +5,22 @@ describe DoctorMailer do
     let(:doctor) {FactoryGirl.create(:doctor, :password_reset_token => "qwerty")}
     let(:mail) { DoctorMailer.password_reset(doctor) }
 
-    it "renders the headers" do
+    it "renders the headers and the reset link" do
       mail.subject.should eq("Password Reset")
       mail.to.should eq(["doctor@example.com"])
-      mail.from.should eq(["from@example.com"])
+      mail.from.should eq(["admin@tredule.com"])
       mail.body.encoded.should match(edit_password_reset_path(doctor.password_reset_token))
+    end
+  end
+  describe 'signup confirmation' do
+    let(:doctor) {FactoryGirl.create(:doctor, :password_reset_token => "qwerty")}
+    let(:mail) { DoctorMailer.signup_confirmation(doctor) }
+
+    it "renders the headers and the welcome message" do
+      mail.subject.should eq("Signup Confirmation")
+      mail.to.should eq(["doctor@example.com"])
+      mail.from.should eq(["admin@tredule.com"])
+      mail.body.encoded.should match("Welcome to Tredule")
     end
   end
 end
